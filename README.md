1446f00559ef17d526afbf81abf2129e66744aa8
zhangxlly <zhangxlly@163.com>
2017-07-15 23:39:19 +0800

    Signed-off-by: zhangxlly <zhangxlly@163.com>

---
+++ README.md
@@ -0,0 +1,107 @@
+### YMP-SSO (Single Sign-On)
+
+> 单点登录服务模块，特性如下：
+> 
+> - 仅一个拦截器搞定单点登录；
+> - 支持Cookies、请求头或请求参数临时存储授权令牌；
+> - 支持服务端和客户端两种模式；
+> - 支持多种跨域身份验证方式；
+> - 支持通过Ajax调用RESTFul API接口的身份验证；
+> - 支持授权令牌加解密；
+
+#### Maven包依赖
+
+    <dependency>
+        <groupId>net.ymate.module</groupId>
+        <artifactId>ymate-module-sso</artifactId>
+        <version>1.0-SNAPSHOT</version>
+    </dependency>
+
+#### 模块配置参数说明
+
+    #-------------------------------------
+    # module.sso 模块初始化参数
+    #-------------------------------------
+    
+    # 在Cookie中存储令牌的名称, 默认值: module.sso_token
+    ymp.configs.module.sso.token_cookie_name=
+    
+    # 令牌存储在请求头中的名称, 默认值: X-ModuleSSO-Token
+    ymp.configs.module.sso.token_header_name=
+    
+    # 令牌URL参数名称, 默认值: token
+    ymp.configs.module.sso.token_param_name=
+    
+    # 令牌生命周期(秒), 默认值为0, 小于等于0表示不启用
+    ymp.configs.module.sso.token_maxage=
+    
+    # 令牌有效性验证的时间间隔(秒), 默认值: 0
+    ymp.configs.module.sso.token_validate_time_interval=
+    
+    # 开启多会话模式(即同一账号允许多处登录), 默认值: false
+    ymp.configs.module.sso.multi_session_enabled=
+    
+    # 开启会话的IP地址检查, 默认值: false
+    ymp.configs.module.sso.ip_check_enabled=
+    
+    # 是否为客户端模式, 默认为false
+    ymp.configs.module.sso.client_mode=
+    
+    # 指定服务端基准URL路径(若客户端模式开启时则此项必填), 必须以'http://'或'https://'开始并以'/'结束, 如: http://www.ymate.net/service/, 默认值: 空
+    ymp.configs.module.sso.service_base_url=
+    
+    # 客户端与服务端之间通讯请求参数签名密钥, 默认值: ""
+    ymp.configs.module.sso.service_auth_key=
+    
+    # 令牌分析适配器接口实现, 默认值: net.ymate.module.sso.impl.DefaultSSOTokenAdapter
+    ymp.configs.module.sso.token_adapter_class=
+    
+    # 令牌存储适配器接口实现, 非客户端模式时必选参数, 默认值: 空
+    ymp.configs.module.sso.storage_adapter_class=
+    
+    # 用户自定义属性加载适配器接口实现, 非客户端模式时有效, 默认值: 空
+    ymp.configs.module.sso.attribute_adapter_class=
+    
+    # 与UserSessionBean整合时, 必须设置此参数
+    ymp.params.webmvc.user_session_handler_class=net.ymate.module.sso.support.SSOUserSessionHandler
+
+#### 示例代码：
+
+- 配置用户会话检查拦截器，该拦截器已集成单点登录相关处理逻辑：
+
+        @RequestMapping(value = "/user/profile/edit", method = Type.HttpMethod.POST)
+        @Before(UserSessionCheckInterceptor.class)
+        public IView __doEditUserProfile(@RequestParam String nickName, ......) throws Exception {
+            // ...... 省略
+            return WebResult.SUCCESS().toJSON();
+        }
+
+- 通过代码操作用户的登录授权令牌对象：
+
+        ISSOToken _token = SSO.get().currentToken();
+        if (_token == null) {
+            // 若返回值为空则表示令牌对象不存在，创建令牌需要传入用户唯一标识Id
+            _token = SSO.get().createToken("uid_xxx");
+        }
+        if (_token != null) {
+            // 令牌唯一标识Id
+            _token.getId();
+            // 用户唯一标识Id
+            _token.getUid();
+            // 用户IP地址
+            _token.getRemoteAddr();
+            // 用户代理信息
+            _token.getUserAgent();
+            // 令牌对象创建时间
+            _token.getCreateTime();
+            // 令牌扩展属性
+            _token.getAttributes();
+        }
+
+#### One More Thing
+
+YMP不仅提供便捷的Web及其它Java项目的快速开发体验，也将不断提供更多丰富的项目实践经验。
+
+感兴趣的小伙伴儿们可以加入 官方QQ群480374360，一起交流学习，帮助YMP成长！
+
+了解更多有关YMP框架的内容，请访问官网：http://www.ymate.net/
---
+++ src/main/java/net/ymate/module/sso/impl/DefaultModuleCfg.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright 2007-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.ymate.module.sso.impl;
+
+import net.ymate.module.sso.*;
+import net.ymate.platform.core.YMP;
+import net.ymate.platform.core.lang.BlurObject;
+import net.ymate.platform.core.util.ClassUtils;
+import org.apache.commons.lang.StringUtils;
+
+import java.util.Map;
+
+/**
+ * @author 刘镇 (suninformation@163.com) on 2017/02/26 上午 03:19
+ * @version 1.0
+ */
+public class DefaultModuleCfg implements ISSOModuleCfg {
+
+    private String __tokenCookieName;
+
+    private String __tokenHeaderName;
+
+    private String __tokenParamName;
+
+    private int __tokenMaxage;
+
+    private int __tokenValidateTimeInterval;
+
+    private String __cacheNamePrefix;
+
+    private boolean __multiSessionEnabled;
+
+    private boolean __ipCheckEnabled;
+
+    private boolean __isClientMode;
+
+    private String __serviceBaseUrl;
+
+    private String __serviceAuthKey;
+
+    private ISSOTokenAdapter __tokenApater;
+
+    private ISSOTokenStorageAdapter __tokenStorageAdapter;
+
+    private ISSOTokenAttributeAdapter __tokenAttributeAdapter;
+
+    public DefaultModuleCfg(YMP owner) {
+        Map<String, String> _moduleCfgs = owner.getConfig().getModuleConfigs(ISSO.MODULE_NAME);
+        //
+        __tokenCookieName = StringUtils.defaultIfBlank(_moduleCfgs.get("token_cookie_name"), ISSO.MODULE_NAME + "_token");
+        //
+        __tokenHeaderName = StringUtils.defaultIfBlank(_moduleCfgs.get("token_header_name"), "X-ModuleSSO-Token");
+        //
+        __tokenParamName = StringUtils.defaultIfBlank(_moduleCfgs.get("token_param_name"), "token");
+        //
+        __tokenMaxage = BlurObject.bind(_moduleCfgs.get("token_maxage")).toIntValue();
+        //
+        __tokenValidateTimeInterval = BlurObject.bind(_moduleCfgs.get("token_validate_time_interval")).toIntValue();
+        //
+        __cacheNamePrefix = StringUtils.trimToEmpty(_moduleCfgs.get("cache_name_prefix"));
+        //
+        __multiSessionEnabled = BlurObject.bind(_moduleCfgs.get("multi_session_enabled")).toBooleanValue();
+        //
+        __ipCheckEnabled = BlurObject.bind(_moduleCfgs.get("ip_check_enabled")).toBooleanValue();
+        //
+        __isClientMode = BlurObject.bind(_moduleCfgs.get("client_mode")).toBooleanValue();
+        //
+        __serviceAuthKey = StringUtils.trimToEmpty(_moduleCfgs.get("service_auth_key"));
+        //
+        if (__isClientMode) {
+            __serviceBaseUrl = StringUtils.trimToNull(_moduleCfgs.get("service_base_url"));
+            if (__serviceBaseUrl != null) {
+                if (!StringUtils.startsWithIgnoreCase(__serviceBaseUrl, "http://") &&
+                        !StringUtils.startsWithIgnoreCase(__serviceBaseUrl, "https://")) {
+                    throw new IllegalArgumentException("The parameter service_base_url is invalid");
+                } else if (!StringUtils.endsWith(__serviceBaseUrl, "/")) {
+                    __serviceBaseUrl = __serviceBaseUrl + "/";
+                }
+            }
+        }
+        //
+        __tokenApater = ClassUtils.impl(_moduleCfgs.get("token_adapter_class"), ISSOTokenAdapter.class, getClass());
+        if (__tokenApater == null) {
+            __tokenApater = new DefaultSSOTokenAdapter();
+        }
+        //
+        __tokenStorageAdapter = ClassUtils.impl(_moduleCfgs.get("storage_adapter_class"), ISSOTokenStorageAdapter.class, getClass());
+        if (!__isClientMode && __tokenStorageAdapter == null) {
+            throw new IllegalArgumentException("The parameter storage_adapter_class is invalid");
+        }
+        //
+        if (!__isClientMode) {
+            __tokenAttributeAdapter = ClassUtils.impl(_moduleCfgs.get("attribute_adapter_class"), ISSOTokenAttributeAdapter.class, getClass());
+        }
+    }
+
+    public String getTokenCookieName() {
+        return __tokenCookieName;
+    }
+
+    public String getTokenHeaderName() {
+        return __tokenHeaderName;
+    }
+
+    public String getTokenParamName() {
+        return __tokenParamName;
+    }
+
+    public int getTokenMaxage() {
+        return __tokenMaxage;
+    }
+
+    public int getTokenValidateTimeInterval() {
+        return __tokenValidateTimeInterval;
+    }
+
+    public String getCacheNamePrefix() {
+        return __cacheNamePrefix;
+    }
+
+    public boolean isMultiSessionEnabled() {
+        return __multiSessionEnabled;
+    }
+
+    public boolean isIpCheckEnabled() {
+        return __ipCheckEnabled;
+    }
+
+    public boolean isClientMode() {
+        return __isClientMode;
+    }
+
+    public String getServiceBaseUrl() {
+        return __serviceBaseUrl;
+    }
+
+    public String getServiceAuthKey() {
+        return __serviceAuthKey;
+    }
+
+    public ISSOTokenAdapter getTokenAdapter() {
+        return __tokenApater;
+    }
+
+    public ISSOTokenStorageAdapter getTokenStorageAdapter() {
+        return __tokenStorageAdapter;
+    }
+
+    public ISSOTokenAttributeAdapter getTokenAttributeAdapter() {
+        return __tokenAttributeAdapter;
+    }
+}
